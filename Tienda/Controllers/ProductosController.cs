a3829041232a0711ef3d3b71eb142861992af0c2
XxXBerserkerGod <berserker98bt@gmail.com>
2023-04-06 10:08:46 -0500

    Controladores

---
+++ Tienda/Controllers/ProductosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tienda.Controllers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
